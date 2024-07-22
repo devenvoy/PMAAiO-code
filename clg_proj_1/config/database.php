d78cf7ba3edadc37c3055ebce19ed34e35fd9124
Nirav Rangapariya <nirav.argon@gmail.com>
2024-07-22 18:09:12 +0530

    add  data to database now

---
+++ clg_proj_1/config/database.php
@@ -0,0 +1,8 @@
+<?php 
+
+define("DB_HOST","localhost");
+define("DB_USER","dev");
+define("DB_PASS","136974");
+define("DB_NAME","php_dev");
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
---
+++ clg_proj_1/model/User.php
@@ -0,0 +1,37 @@
+<?php
+
+class User {
+    public int $id;
+    public string $first_name;
+    public string $last_name;
+    public int $age;
+    public string $address;
+    public string $email;
+    public string $password;
+    public string $first_created;
+    public string $last_updated;
+
+    public function __construct(
+        int $id,
+        string $first_name,
+        string $last_name,
+        int $age,
+        string $address,
+        string $email,
+        string $password,
+        string $first_created,
+        string $last_updated
+    ) {
+        $this->id = $id;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->age = $age;
+        $this->address = $address;
+        $this->email = $email;
+        $this->password = $password;
+        $this->first_created = $first_created;
+        $this->last_updated = $last_updated;
+    }
+}
+
+?>
